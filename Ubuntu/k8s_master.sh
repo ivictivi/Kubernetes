5c30fd755a7dbeda3be8dd3e61d4b9768b9a4d76
ivictivi <nachogarcia.mcs@gmail.com>
2021-07-22 22:31:38 +0200

    Fichero de configuración K8s

--- Ubuntu/k8s_master.sh
+++ Ubuntu/k8s_master.sh
@@ -1,38 +1,111 @@
 #!/bin/bash
-# Añadir repositorio de Docker
+## FUNCIONES
+main () {
 if [[ "$EUID" = 0 ]]; then
     echo "[INFO] EJECUTANDO COMO ROOT"
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-    # Añadir repositorio de Kubernetes
-    cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-    deb https://apt.kubernetes.io/ kubernetes-xenial main
-    EOF
-
-    # Actualizar e instalar paquetes
-    sudo apt update
-    sudo apt install -y docker-ce=5:19.03.10~3-0~ubuntu-focal kubelet=1.18.5-00 kubeadm=1.18.5-00 kubectl=1.18.5-00
-
-    #En el Master arrancarlo como Master
-    sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-    # Si da error, consultar esta guía con la parte de Docker. 
-    # https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
-
-    # Crear directorios de trabajo
-    sudo mkdir -p $HOME/.kube
-    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-    sudo chown $(id -u):$(id -g) $HOME/.kube/config
-    kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+    node_config
 else
     sudo -k # make sure to ask for password on next sudo
     if sudo true; then
-        echo "(2) correct password"
+        echo "[INFO] EJECUTANDO COMO ROOT"
+        node_config
     else
-        echo "(3) wrong password"
+        echo "[ERROR] ECUTANDO COMO NO ROOT"
         exit 1
     fi
 fi
+
+}
+
+node_config () {
+    ##### Disable Firewall
+    ufw disable
+    ##### Disable swap
+    swapoff -a; sed -i '/swap/d' /etc/fstab
+    ##### Update sysctl settings for Kubernetes networking
+    cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+    net.bridge.bridge-nf-call-ip6tables = 1
+    net.bridge.bridge-nf-call-iptables = 1
+EOF
+    sysctl --system
+    ##### Install docker engine
+    apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt update
+    apt install -y docker-ce=5:19.03.10~3-0~ubuntu-focal containerd.io
+
+    ### Kubernetes Setup
+    ##### Add Apt repository
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+    ##### Install Kubernetes components
+    apt update && apt install -y kubeadm=1.18.5-00 kubelet=1.18.5-00 kubectl=1.18.5-00
+
+    ##### In case you are using LXC containers for Kubernetes nodes
+    mknod /dev/kmsg c 1 11
+    echo '#!/bin/sh -e' >> /etc/rc.local
+    echo 'mknod /dev/kmsg c 1 11' >> /etc/rc.local
+    chmod +x /etc/rc.local
+    check_node
+}
+
+check_node () {
+node_type="0"
+while [ $node_type -eq 0 ]
+do
+    read -p "Selecciona el tipo de nodo: master (1) o worker (2): " node_type
+    if [ $node_type -eq 1 ];then
+        echo "[INFO] Has elegido el nodo master."
+        master_config
+    elif [ $node_type -eq 2 ];then
+        echo "[INFO] Has elegido el nodo worker."
+        worker_config
+    else
+        echo ""
+        echo "[ERROR] El nodo elegido no es correcto."
+        node_type="0"
+    fi
+done
+}
+
+
+master_config () {
+    ## On kmaster
+    ##### Initialize Kubernetes Cluster
+    kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16  --ignore-preflight-errors=all
+
+    ##### Deploy Calico network
+    kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+
+    ##### Cluster join command
+    kubeadm token create --print-join-command
+
+    ##### To be able to run kubectl commands as non-root user
+    #mkdir -p $HOME/.kube
+    #sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    #sudo chown $(id -u):$(id -g) $HOME/.kube/config
+}
+
+worker_config () {
+    echo "[INFO] Introduce el token generado en el Master"
+}
+
+
+
+
+
+
+## EJECUCIOn
+main
+
+
+
+
+
+
+
+
 
 
